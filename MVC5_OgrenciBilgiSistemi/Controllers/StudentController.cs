a55dbcb58328967e3ecae19715662ace816cf9a7
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2022-05-04 22:29:30 +0300

    created

---
+++ MVC5_OgrenciBilgiSistemi/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using MVC5_OgrenciBilgiSistemi.Models;
+using System.Linq;
+using System.Web.Mvc;
+namespace Gym_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        MVC5_OgrenciBilgiSistemi.Models.SCHOOLEntities1 db = new MVC5_OgrenciBilgiSistemi.Models.SCHOOLEntities1();
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            var ogrliste = db.Students.ToList();
+
+            return View(ogrliste);
+
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Student S)
+        {
+            db.Students.Add(S);
+            db.SaveChanges();
+            return View();
+        }
+
+
+    }
+}
